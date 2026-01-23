602b7f591e3c1a20273a4f7755dbddcce2fa5e59
1G-N0R <Aadhithya.Padmanaban@gmail.com>
2026-01-23 11:05:11 -0500

    Update AutonRealLeft.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/AutonRealLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/AutonRealLeft.java
@@ -243,8 +243,8 @@ public class AutonRealLeft extends OpMode {
 
                         //IntakeInner.setVelocity(0);
                         //IntakeOuter.setVelocity(0);
-                        flywheelLeft.setVelocity(-.01);
-                        flywheelRight.setVelocity(-.01);
+//                        flywheelLeft.setVelocity(-.01);
+//                        flywheelRight.setVelocity(-.01);
                         if (count == 1) {
 
                             follower.followPath(grabPickup1, true);
